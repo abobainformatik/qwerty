f4af1217363cb8ea67aaaf2e55e50815ad2d48f9
abobainformatik <sherbakovmaximka53@gmail.com>
2025-04-03 11:19:16 +0300

    Update Homework 2.5.py

--- Homework 2.5.py
+++ Homework 2.5.py
@@ -1,51 +1,47 @@
-import matplotlib.pyplot as plt
 import numpy as np
+import matplotlib.pyplot as plt
 
 class Derivative:
-    def init(self, h=1e-5):
-        self.h = h
+    def __init__(self, h=1e-5):
+        self.h = h #Шаг для вычисления производной
 
-    def get(self, instance, owner):
+    def __call__(self, func, x):
+        """Вычисляет производную функции func в точке x."""
+        return (func(x + self.h) - func(x - self.h)) / (2 * self.h)
+
+    def __get__(self, instance, owner):
+        """Дескриптор: позволяет вызывать производную как атрибут класса ExponentialFunction."""
         if instance is None:
-            return self  # Возвращаем сам дескриптор, если доступ через класс
-
-        self.instance = instance  # Сохраняем ссылку на экземпляр
-
-        return self  # Возвращаем сам дескриптор
-
-    def call(self, x):
-        if self.instance is None:
-             raise AttributeError("Derivative must be accessed via an ExponentialFunction instance.")
-        return (self.instance(x + self.h) - self.instance(x - self.h)) / (2 * self.h)
-
+            return self
+        return lambda x: self(instance, x)
 
 class ExponentialFunction:
-    def init(self, a=1):
+    def __init__(self, a):
         self.a = a
-        self.derivative = Derivative()  # Создаем экземпляр дескриптора
+        self.derivative = Derivative() # Атрибут, являющийся экземпляром Derivative
 
-    def call(self, x):
+    def __call__(self, x):
+        """Вычисляет значение функции f(x) = a * e^x."""
         return self.a * np.exp(x)
 
-    def plot(self, x_range=(-2, 2), num_points=100):
-        x = np.linspace(x_range[0], x_range[1], num_points)
-        y = [self(xi) for xi in x]
-        y_prime = [self.derivative(xi) for xi in x] # Используем дескриптор правильно
 
-        plt.plot(x, y, label=f'f(x) = {self.a} * e^x')
-        plt.plot(x, y_prime, label=f"f'(x) = {self.a} * e^x")
-        plt.xlabel('x')
-        plt.ylabel('y')
-        plt.title('График функции и её производной')
-        plt.legend()
-        plt.grid(True)
-        plt.show()
+# Пример использования и построение графиков:
+a = 2 # Коэффициент перед экспонентой
+f = ExponentialFunction(a)
 
+# Интервал для графиков:
+x = np.linspace(-2, 2, 100)
+# Вычисляем значения функции и ее производной:
+y = [f(xi) for xi in x]
+y_derivative = [f.derivative(xi) for xi in x]
 
-# Пример использования
-exp_func = ExponentialFunction(a=2)
-print(exp_func(0))
-print(exp_func.derivative(0)) # Используем дескриптор правильно
-
-# Построение графиков
-exp_func.plot()
+# Строим графики:
+plt.figure(figsize=(10, 6))
+plt.plot(x, y, label=f'f(x) = {a} * e^x')
+plt.plot(x, y_derivative, label="f'(x) =  Производная") #Сделана подпись, что это производная.
+plt.xlabel("x")
+plt.ylabel("y")
+plt.title("График функции и ее производной")
+plt.legend()
+plt.grid(True)
+plt.show()
